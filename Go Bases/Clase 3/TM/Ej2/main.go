7a5da47d80c6159765fe0a685368bafd188c5795
franvilardo <franco.vilardo@mercadolibre.com>
2023-03-28 17:06:08 -0300

    TM 28/03

---
+++ Go Bases/Clase 3/TM/Ej2/main.go
@@ -0,0 +1,42 @@
+// Una empresa necesita realizar una buena gestión de sus empleados, para esto realizaremos un pequeño programa nos ayudará a gestionar correctamente dichos empleados. Los objetivos son:
+// Crear una estructura Person con los campos ID, Name, DateOfBirth.
+// Crear una estructura Employee con los campos: ID, Position y una composicion con la estructura Person.
+// Realizar el método a la estructura Employe que se llame PrintEmployee(), lo que hará es realizar la impresión de los campos de un empleado.
+// Instanciar en la función main() tanto una Person como un Employee cargando sus respectivos campos y por último ejecutar el método PrintEmployee().
+// Si logras realizar este pequeño programa pudiste ayudar a la empresa a solucionar la gestión de los empleados.
+
+package main
+
+import "fmt"
+
+func main() {
+	newPerson := Person{
+		ID:          1,
+		Name:        "Pedro Gomez",
+		DateOfBirth: 12061994,
+	}
+	newEmployee := Employee{
+		ID:       1,
+		Position: "Manager",
+		Person:   newPerson,
+	}
+
+	newEmployee.PrintEmployee()
+
+}
+
+type Person struct {
+	ID          int
+	Name        string
+	DateOfBirth int
+}
+
+type Employee struct {
+	ID       int
+	Position string
+	Person   Person
+}
+
+func (employee Employee) PrintEmployee() {
+	fmt.Println(employee)
+}
