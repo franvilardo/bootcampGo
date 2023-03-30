9b2008cc0be1e8f6d411d8b08c76f4fc766b38de
franvilardo <franco.vilardo@mercadolibre.com>
2023-03-30 10:57:17 -0300

    agregado desafio

---
+++ Go_Bases/Clase 4/TM/Ej2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+)
+
+type myError struct {
+	message string
+}
+
+func (e *myError) Error() string {
+	return fmt.Sprintf("Error: %s", e.message)
+}
+
+func main () {
+	var salary int = 8000
+
+	if salary < 10000 {
+		myErr := &myError{message: "el salario ingresado no alcanza el mínimo imponible"}
+		var err *myError
+		if errors.Is(myErr, err) {
+			fmt.Println(myErr.message)
+		}
+type myError struct {
+	message string
+}
+
+func (e *myError) Error() string {
+	return fmt.Sprintf("Error: %s", e.message)
+}
+
+func validate(salary int)error {
+	if salary < 10000 {
+		return errors.New("Error: el salario es menor a 10.000")
+	}
+	return nil
+}
+
+func main () {
+	var salary int = 8000
+	if condition {
+		
+	}
+}
