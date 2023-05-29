59030d8e1bd0dcb3df06c74f5e2a6f421ab6d7aa
Franco Vilardo <fvilardo@uy000xj3xwr2ny6.ml.com>
2023-05-29 12:39:37 -0300

    ejs nuevos

---
+++ Go_Bases/Clase1/TM/Ej3/main.go
@@ -0,0 +1,23 @@
+package main
+
+
+//var 1nombre string
+var nombre1 string
+
+//var apellido string
+var apellido string
+
+//var int edad
+var int edad
+
+//1apellido := 6
+apellido1 := "seis"
+
+//var licencia_de_conducir = true
+var licenciaDeConducir = true
+
+//var estatura de la persona int
+var estaturaDeLaPersona int
+
+//cantidadDeHijos := 2
+cantidadDeHijos := 2
---
+++ Go_Bases/Clase1/TM/Ej4/main.go
@@ -0,0 +1,16 @@
+package main
+
+//var apellido string = "Gomez"
+var apellido string = "Gomez"
+
+//var edad int = "35"
+var edad int = 35
+
+//boolean := "false";
+boolean := false
+
+//var sueldo string = 45857.90
+var sueldo float32 = 45857.90
+
+//var nombre string = "Julián"
+var nombre string = "Julián"
